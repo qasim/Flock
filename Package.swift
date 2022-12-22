8032c241e3c62eea78286abc83d7cd6782c01762
Qasim Iqbal <me@qas.im>
2022-12-22 12:48:58 -0500

    Remove .git prefix from swift-log dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", exact: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", exact: "1.4.4"),
+        .package(url: "https://github.com/apple/swift-log", exact: "1.4.4"),
     ],
     targets: [
         .target(
