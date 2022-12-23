018a2684be1afeb6439ed9e8e8f75a7064137e72
Qasim Iqbal <me@qas.im>
2022-12-22 22:04:23 -0500

    Comments

--- Package.swift
+++ Package.swift
@@ -9,8 +9,11 @@ let package = Package(
         .library(name: "Flock", targets: ["Flock"]),
     ],
     dependencies: [
+        // Source Dependencies
         .package(url: "https://github.com/apple/swift-algorithms", exact: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log", exact: "1.4.4"),
+
+        // Plugins
         .package(url: "https://github.com/themomax/swift-docc-plugin", branch: "add-extended-types-flag"),
     ],
     targets: [
