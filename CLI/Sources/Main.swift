c41b3ac62592c374502dce1af1061beaf85d09f9
Qasim Iqbal <me@qas.im>
2022-12-23 14:37:20 -0500

    Formatting

--- CLI/Sources/Main.swift
+++ CLI/Sources/Main.swift
@@ -12,18 +12,10 @@ struct Main: AsyncParsableCommand {
     @Argument(help: "An URL to download.")
     var remoteSource: String
 
-    @Option(
-        name: .shortAndLong,
-        help: """
-        The maximum number of connections to create in parallel.
-        """
-    )
+    @Option(name: .shortAndLong, help: "The maximum number of connections to create in parallel.")
     var connectionCount: Int = ProcessInfo.processInfo.activeProcessorCount
 
-    @Option(
-        name: .shortAndLong,
-        help: "The minimum size, in bytes, for each connection."
-    )
+    @Option(name: .shortAndLong, help: "The minimum size, in bytes, for each connection.")
     var minimumConnectionSize: Int = 16777216
 
     @Flag(name: .shortAndLong, help: "Whether or not verbose logs should be printed to standard output.")
